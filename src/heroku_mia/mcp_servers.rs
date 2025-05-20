a2607121a7f5f9b661ca774fa111630eca297be7
Terence Lee <hone02@gmail.com>
2025-05-20 01:01:55 -0500

    add enums to mcp servers endpoint

--- src/heroku_mia/mcp_servers.rs
+++ src/heroku_mia/mcp_servers.rs
@@ -10,8 +10,8 @@ pub struct McpServerResponse {
     pub created_at: String,
     pub updated_at: String,
     pub tools: Vec<ToolDetails>,
-    pub server_status: String,
-    pub primitives_status: String,
+    pub server_status: ServerStatus,
+    pub primitives_status: PrimitivesStatus,
     pub namespace: String,
 }
 
@@ -21,5 +21,30 @@ pub struct ToolDetails {
     pub namespaced_name: String,
     pub description: String,
     pub input_schema: Value,
-    pub annotations: Value,
+    pub annotations: Annotations,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Annotations {
+    title: String,
+    read_only_hint: bool,
+    destructive_hint: bool,
+    idempotent_hint: bool,
+    open_world_hint: bool,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum ServerStatus {
+    Registered,
+    Disconnected,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum PrimitivesStatus {
+    Syncing,
+    Synced,
+    Error,
 }
